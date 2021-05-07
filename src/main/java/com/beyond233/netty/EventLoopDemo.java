c9f49ba0c933859b50b23f95fcb4ddef6cb0b1f3
XuJie <414460547@qq.com>
2021-05-07 17:37:47 +0800

    netty学习

---
+++ src/main/java/com/beyond233/netty/EventLoopDemo.java
@@ -0,0 +1,43 @@
+package com.beyond233.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * learn EventLoop
+ *
+ * @author beyond233
+ * @date 2021/5/7 15:19
+ */
+public class EventLoopDemo {
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        new ServerBootstrap()
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                ByteBuf byteBuf = msg instanceof ByteBuf ? ((ByteBuf) msg) : null;
+                                if (byteBuf != null) {
+                                    byte[] buf = new byte[16];
+                                    ByteBuf len = byteBuf.readBytes(buf, 0, byteBuf.readableBytes());
+                                    System.out.println(new String(buf));
+                                }
+                            }
+                        });
+                    }
+                }).bind(8080).sync();
+
+    }
+}
---
+++ src/main/java/com/beyond233/netty/TimeServer.java
@@ -0,0 +1,52 @@
+package com.beyond233.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * netty实现server
+ *
+ * @author beyond233
+ * @date 2021/5/7 17:23
+ */
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 构造NIO线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 构造server启动辅助类
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(workerGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(null);
+            // 绑定端口，同步等待成功
+            ChannelFuture future = server.bind(port).sync();
+            // 等待Server端口关闭
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    private class TimeServerHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            super.channelRead(ctx, msg);
+        }
+    }
+
+}
