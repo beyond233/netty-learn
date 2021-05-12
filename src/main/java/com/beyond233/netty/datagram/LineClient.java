fb7dd82a956712776d47e920b6e19cd116f98100
beyond233 <414460547@qq.com>
2021-05-12 09:03:53 +0800

    LineBasedFrameDecoder解决TCP粘包问题

--- src/main/java/com/beyond233/netty/datagram/LineClient.java
+++ src/main/java/com/beyond233/netty/datagram/LineClient.java
@@ -1,9 +1,9 @@
 package com.beyond233.netty.datagram;
 
 /**
- * todo
+ * 利用LineBasedFrameDecoder解决TCP粘包问题
  *
- * @author XuJie
+ * @author beyond233
  * @date 2021/5/12 9:00
  */
 public class LineClient {
